2b6dd3f8fb88ed3b967d128ff71c583069116e42
Brad Caron <bacaron@iu.edu>
2020-08-09 00:52:53 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,16 @@ fa=`jq -r '.fa' config.json`
 md=`jq -r '.md' config.json`
 ad=`jq -r '.ad' config.json`
 rd=`jq -r '.rd' config.json`
-met="fa md ad rd"
+ga=`jq -r '.ga' config.json`
+ak=`jq -r '.ak' config.json`
+mk=`jq -r '.mk' config.json`
+rk=`jq -r '.rk' config.json`
+
+if [[ ${ga} == 'null' ]]; then
+	met="fa md ad rd"
+else
+	met="fa md ad rd ga mk ak rk"
+fi
 
 outdir="raw"
 
