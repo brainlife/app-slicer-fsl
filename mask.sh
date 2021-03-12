eaeb591cc30a969ee32d6df7b07a0e16a6509b89
Brad Caron <bacaron@iu.edu>
2021-03-12 11:49:19 -0500

    Update mask.sh

--- mask.sh
+++ mask.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-dwi=`jq -r '.dwi' config.json`
-bvals=`jq -r '.bvals' config.json`
+bold=`jq -r '.bold' config.json`
 
-select_dwi_vols ${dwi} ${bvals} nodif 0 -m
+fslselectvols -i ${bold} -o nodif.nii.gz --vols=0
 
 bet nodif.nii.gz nodif_brain -f 0.2 -g 0 -m
 
-mv nodif_brain.nii.gz dwi.nii.gz
+mv nodif_brain.nii.gz bold.nii.gz
 mv nodif_brain_mask.nii.gz mask.nii.gz
