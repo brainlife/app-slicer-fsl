7f5410fc8cff57cfd8bb4a516c900adc29fc3ae4
Brad Caron <bacaron@iu.edu>
2021-03-12 11:47:12 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ outdir="raw"
 mkdir -p $outdir
 
 # render overlay
-fsleyes render -ds "world" -hc -hl -c 2 -ad -xz 500 -yz 500 -zz 500 --outfile ${outdir}/out.png t1.nii.gz dwi.nii.gz -ot volume -a 50 --cmap red-yellow -un mask.nii.gz -ot mask -o -mc 1 1 0
+fsleyes render -ds "world" -hc -hl -c 2 -ad -xz 500 -yz 500 -zz 500 --outfile ${outdir}/out.png t1.nii.gz bold.nii.gz -ot volume -a 50 --cmap red-yellow -un mask.nii.gz -ot mask -o -mc 1 1 0
 
